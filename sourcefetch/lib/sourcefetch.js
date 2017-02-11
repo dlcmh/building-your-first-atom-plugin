16d56f8529e86c58044e01ce41335142a7ae8cd4
David Chin <dlcmhd@me.com>
2017-02-11 11:18:20 +0800

    rename `sourcefetch:toggle` to `sourcefetch:fetch`

--- sourcefetch/lib/sourcefetch.js
+++ sourcefetch/lib/sourcefetch.js
@@ -1,43 +1,26 @@
 'use babel';
 
-import SourcefetchView from './sourcefetch-view';
 import { CompositeDisposable } from 'atom';
 
 export default {
 
-  sourcefetchView: null,
-  modalPanel: null,
   subscriptions: null,
 
   activate(state) {
-    this.sourcefetchView = new SourcefetchView(state.sourcefetchViewState);
-    this.modalPanel = atom.workspace.addModalPanel({
-      item: this.sourcefetchView.getElement(),
-      visible: false
-    });
-
     // Events subscribed to in atom's system can be easily cleaned up with a CompositeDisposable
     this.subscriptions = new CompositeDisposable();
 
     // Register command that toggles this view
     this.subscriptions.add(atom.commands.add('atom-workspace', {
-      'sourcefetch:toggle': () => this.toggle()
+      'sourcefetch:fetch': () => this.fetch()
     }));
   },
 
   deactivate() {
-    this.modalPanel.destroy();
     this.subscriptions.dispose();
-    this.sourcefetchView.destroy();
   },
 
-  serialize() {
-    return {
-      sourcefetchViewState: this.sourcefetchView.serialize()
-    };
-  },
-
-  toggle() {
+  fetch() {
     let editor
     if (editor = atom.workspace.getActiveTextEditor()) {
       let selection = editor.getSelectedText()
